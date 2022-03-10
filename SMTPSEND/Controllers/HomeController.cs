621e0f72b68f48597dbbe6b4f17964536ac78aeb
TinZawWin <thetinzawwin@gmail.com>
2022-03-10 16:41:10 +0630

    Add project files.

---
+++ SMTPSEND/Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SMTPSEND.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public bool SendEmail(string sendermail,string password,string receivermail)
+        {
+            string smtpAddress = "smtp.gmail.com";
+            int portNumber = 587;
+            bool enableSSL = true;
+            string subject = string.Empty;
+            string body = string.Empty;
+           
+                subject = " application have been approved";
+                body = "<p>Dear xxxx,</p><p> Your  application has been approved.</p><p> Regards,<br/> xxxxxxx <br/> Sender </p> ";
+
+
+            using (MailMessage mail = new MailMessage())
+            {
+                mail.From = new MailAddress(sendermail);
+                mail.To.Add(receivermail);
+                mail.Subject = subject;
+                mail.Body = body;
+                mail.IsBodyHtml = true;
+              
+                //attached file
+                mail.Attachments.Add(new Attachment("D:/Tin Zaw Win/deadlinechangequery.txt"));
+            
+                using (SmtpClient smtp = new SmtpClient(smtpAddress, portNumber))
+                {
+                    smtp.Credentials = new NetworkCredential(sendermail, password);
+                    smtp.EnableSsl = enableSSL;
+                    smtp.Send(mail);
+                }
+                return true;
+            }
+        }
+    }
+}
---
+++ SMTPSEND/Scripts/js/send.js
@@ -0,0 +1,25 @@
+﻿var token = $("input[name=__RequestVerificationToken]").val();
+$("#Send").on("click", function () {
+    var send = $("#Sender").val();
+    var password = $("#Password").val();
+    var receive = $("#Receiver").val();
+$.ajax({
+        url: '/Home/SendEmail',
+        type: 'POST',
+        dataType: 'json',
+    data: { "__RequestVerificationToken": token, "sendermail": send, "password": password, "receivermail": receive },
+        success: function (data) {
+
+            $("#piname").text("Name of PI: "+data.NAME);
+            $("#designation").text("Designation: "+data.Designation);
+            $("#cluster").text("Cluster/Department: "+data.Department);
+            $("#institution").text("Institution: "+data.Institution);
+            $("#teleno").text("Telephone Number: "+data.TELENO);
+            $("#mail").text("Mailing Address: "+data.ADDRESS);
+            $("#email").text("Email Address:"+data.EMAIL);
+        },
+        error: function (xhr) {
+        }
+    });
+
+});
